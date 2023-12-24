7f593522ee3fc31acf8218d3599c23aa148b9d8a
Vaishakh GK <vaishakhgk2006@gmail.com>
2023-12-24 08:42:44 +0530

    Go version updated, Simplified main.go

--- cmd/main.go
+++ cmd/main.go
@@ -11,34 +11,40 @@ import (
 )
 
 func main() {
+	// Load enviornment variables
 	godotenv.Load(".env")
 	var amqpUrl = os.Getenv("AMQP_URL")
 
-	var connectRabbitMq,err = amqp.Dial(amqpUrl)
+	// connecting to rabbit mq
+	var connectRabbitMq, err = amqp.Dial(amqpUrl)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer connectRabbitMq.Close()
 
-	channel,err := connectRabbitMq.Channel()
+	// Connecting rabbit mq channel
+	channel, err := connectRabbitMq.Channel()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer channel.Close()
 
+	// Creating rabbit mq channel
 	channel.QueueDeclare(
 		"send-email",
 		true,
-		true,
+		false,
 		false,
 		false,
 		nil,
 	)
-	
-	messages,err := channel.Consume(
+	channel.Qos(1, 0, false)
+
+	// Consuming messages from rabbit mq
+	messages, err := channel.Consume(
 		"send-email",
 		"",
-		true,
+		false,
 		false,
 		false,
 		false,
@@ -48,11 +54,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for {
-		for msg := range messages {
-			var m map[string]interface{}
-			json.Unmarshal(msg.Body,&m)
-			fmt.Println(m)
-		}
+	// Get messages from rabbiq mq channel
+	var forver = make(chan bool)
+	GetMessages(messages)
+	<-forver
+}
+
+func GetMessages(messages <-chan amqp.Delivery) {
+	for msg := range messages {
+		var m map[string]interface{}
+		json.Unmarshal(msg.Body, &m)
+		fmt.Println(m)
+		msg.Ack(false)
 	}
 }
